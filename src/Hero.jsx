6f5402a2de10bd26fd20c8bca23c5b056b8e3cd5
sksandeep4 <40865955+sksandeep4@users.noreply.github.com>
2023-11-14 09:49:38 +0530

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="hero py-10">
       <div className="image">
-        <img src={"src/images.png"} alt="Sandeep.jpg" className="propic" />
+        <img src={"https://github.com/sksandeep4/portfolio/blob/main/src/images.png?raw=true"} alt="Sandeep.jpg" className="propic" />
       </div>
       <div className="container">
         <p /*style="float: right"*/>
